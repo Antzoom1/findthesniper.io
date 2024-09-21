a2aa7e9659289d619b1bd99a44cb75034f23bee1
RagOfJoes <apps@ragofjoes.dev>
2024-09-21 00:28:03 -0700

    refactor(api): consolidate image into challenge
    * no real reason why they should be separate

--- api/domains/challenge.go
+++ api/domains/challenge.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/RagOfJoes/findthesniper.io/internal"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"github.com/uptrace/bun"
 )
 
@@ -19,6 +20,22 @@ type Challenge struct {
 	// Prompt defines the prompt for the game
 	Prompt string `bun:"type:varchar(256),notnull" json:"prompt"`
 
+	// ImageURL defines the ImageURL for the image for the challenge
+	ImageURL string `bun:"type:text,notnull" json:"url"`
+	// ImageHeight defines the original height of the image for the challenge
+	ImageHeight float32 `bun:"type:float,notnull" json:"height"`
+	// ImageWidth defines the original width of the image for the challenge
+	ImageWidth float32 `bun:"type:float,notnull" json:"width"`
+
+	// SniperHeight defines the height of the sniper object in the image relative to the original image's dimensions
+	SniperHeight float32 `bun:"type:float,notnull" json:"sniper_height"`
+	// SniperWidth defines the width of the sniper object in the image relative to the original image's dimensions
+	SniperWidth float32 `bun:"type:float,notnull" json:"sniper_width"`
+	// SniperX defines the x coordinate of the center of the circle in the image relative to the original image's dimensions
+	SniperX float32 `bun:"type:float,notnull" json:"sniper_x"`
+	// SniperY defines the y coordinate of the center of the circle in the image relative to the original image's dimensions
+	SniperY float32 `bun:"type:float,notnull" json:"sniper_y"`
+
 	// CreatedAt defines when the user was created
 	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp" json:"created_at"`
 	// UpdatedAt defines when the user was last updated
@@ -26,11 +43,7 @@ type Challenge struct {
 	// DeletedAt defines when and if the user was deleted
 	DeletedAt bun.NullTime `bun:",soft_delete,nullzero,default:NULL" json:"-"`
 
-	// ImageID defines the unique id of the image for the challenge
-	ImageID string `bun:"type:varchar(26)" json:"-"`
-	// Image is the image for the challenge
-	Image Image `bun:"rel:belongs-to,join:image_id=id" json:"image"`
-
+	// UserID defines unique id for the user that created the challenge
 	UserID string `bun:"type:varchar(26)" json:"-"`
 	// User is the user that created the challenge
 	User User `bun:"rel:belongs-to,join:user_id=id" json:"user"`
@@ -40,13 +53,19 @@ func (c Challenge) Validate() error {
 	return validation.ValidateStruct(&c,
 		validation.Field(&c.ID, validation.Required, validation.By(internal.IsULID)),
 		validation.Field(&c.Prompt, validation.Required, validation.Length(1, 256)),
+		validation.Field(&c.ImageURL, validation.Required, is.URL),
+
+		validation.Field(&c.ImageHeight, validation.Required, validation.Min(0)),
+		validation.Field(&c.ImageWidth, validation.Required, validation.Min(0)),
+
+		validation.Field(&c.SniperHeight, validation.Required, validation.Max(c.ImageHeight), validation.Min(0)),
+		validation.Field(&c.SniperWidth, validation.Required, validation.Max(c.ImageWidth), validation.Min(0)),
+		validation.Field(&c.SniperX, validation.Required, validation.Min(c.ImageWidth-c.SniperWidth/2), validation.Min(c.SniperWidth/2)),
+		validation.Field(&c.SniperY, validation.Required, validation.Max(c.ImageHeight-c.SniperHeight/2), validation.Min(c.SniperHeight/2)),
 
 		validation.Field(&c.CreatedAt, validation.Required),
 		validation.Field(&c.UpdatedAt, validation.When(!c.UpdatedAt.IsZero(), validation.By(internal.IsAfter(c.CreatedAt)))),
 		validation.Field(&c.DeletedAt, validation.When(!c.DeletedAt.IsZero(), validation.By(internal.IsAfter(c.CreatedAt)))),
-
-		validation.Field(&c.ImageID, validation.Required, validation.By(internal.IsULID)),
-		validation.Field(&c.Image, validation.Required),
 
 		validation.Field(&c.UserID, validation.Required, validation.By(internal.IsULID)),
 		validation.Field(&c.User, validation.Required),
